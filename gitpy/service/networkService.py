e9673239d15814efd04246e8586c98a1012ee8ce
Akshay Kathpal <21139206+akk29@users.noreply.github.com>
2025-05-02 19:08:59 +0530

    perf: ⚡ performance improvements
    1.  moved from request to session based API calls

--- gitpy/service/networkService.py
+++ gitpy/service/networkService.py
@@ -2,20 +2,21 @@ import requests, json
 class NetworkService:
 
     def __init__(self, headers=None):
-        self.headers = headers
+        self.session = requests.Session()
+        self.session.headers.update(headers)
 
     def get(self, url):
-        response = requests.get(url, headers=self.headers)
+        response = self.session.get(url)
         return response
 
     def post(self, url, payload):
-        response = requests.post(url, headers=self.headers, data=json.dumps(payload))
+        response = self.session.post(url, data=json.dumps(payload))
         return response
 
     def delete(self, url,payload):
-        response = requests.delete(url,headers=self.headers, data=json.dumps(payload))
+        response = self.session.delete(url, data=json.dumps(payload))
         return response
 
     def update(self, url, payload):
-        response = requests.put(url, headers=self.headers, data=json.dumps(payload))
+        response = self.session.put(url, data=json.dumps(payload))
         return response
